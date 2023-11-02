cfd123fb32d9f5b888577a71fd338d41089d014e
samibm23 <94871825+samibm23@users.noreply.github.com>
2023-11-02 21:02:01 +0100

    Update DepartementServiceImplTest.java

--- src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
@@ -1,29 +1,29 @@
-package tn.esprit.spring.service;
+// package tn.esprit.spring.service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
-import tn.esprit.kaddemproject.KaddemProjectApplication;
-import tn.esprit.kaddemproject.entities.Departement;
-import tn.esprit.kaddemproject.services.IDepartementService;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+// import org.junit.jupiter.api.Order;
+// import org.junit.jupiter.api.TestMethodOrder;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Import;
+// import tn.esprit.kaddemproject.KaddemProjectApplication;
+// import tn.esprit.kaddemproject.entities.Departement;
+// import tn.esprit.kaddemproject.services.IDepartementService;
 
-import java.util.List;
-@SpringBootTest (classes = {KaddemProjectApplication.class})
-@TestMethodOrder(OrderAnnotation.class)
+// import java.util.List;
+// @SpringBootTest (classes = {KaddemProjectApplication.class})
+// @TestMethodOrder(OrderAnnotation.class)
 
-class DepartementServiceImplTest {
-    @Autowired
-    IDepartementService ds;
-    @Test
-    @Order(1)
-    public void testRetrieveAll() {
-        List<Departement> listDepartements = ds.retrieveAll();
-        Assertions.assertEquals(0, listDepartements.size());
-    }
+// class DepartementServiceImplTest {
+//     @Autowired
+//     IDepartementService ds;
+//     @Test
+//     @Order(1)
+//     public void testRetrieveAll() {
+//         List<Departement> listDepartements = ds.retrieveAll();
+//         Assertions.assertEquals(0, listDepartements.size());
+//     }
 
-}
+// }
