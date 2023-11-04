88021bd7103334181e3862c5726bcfff539fb07d
samibm23 <94871825+samibm23@users.noreply.github.com>
2023-11-04 15:08:36 +0100

    Update DepartementServiceImplTest.java

--- src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
@@ -63,7 +63,7 @@ class DepartementServiceImplTest {
 @Order(4)
 public void testDeleteDepartement() {
     // Replace 'id' with a valid Departement ID from your database
-        Integer id = 4;
+        Integer id = 5;
     Boolean isDeleted = ds.delete(id);
     Assertions.assertTrue(isDeleted);
 }
