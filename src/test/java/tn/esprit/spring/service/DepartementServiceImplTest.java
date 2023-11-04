d8f014b76ec04f9e62477fe76c896d120f48911c
samibm23 <94871825+samibm23@users.noreply.github.com>
2023-11-04 14:25:21 +0100

    'tests'

--- src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
@@ -10,9 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import tn.esprit.kaddemproject.KaddemProjectApplication;
 import tn.esprit.kaddemproject.entities.Departement;
+import tn.esprit.kaddemproject.entities.Etudiant;
+import tn.esprit.kaddemproject.entities.Option;
 import tn.esprit.kaddemproject.services.IDepartementService;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static tn.esprit.kaddemproject.entities.Option.GAMIX;
+
 @SpringBootTest (classes = {KaddemProjectApplication.class})
 @TestMethodOrder(OrderAnnotation.class)
 
@@ -25,5 +32,24 @@ class DepartementServiceImplTest {
         List<Departement> listDepartements = ds.retrieveAll();
         Assertions.assertEquals(0, listDepartements.size());
     }
+    @Test
+    @Order(2)
+    public void testAddDepartement() {
+        Departement newDepartement = new Departement();
+        newDepartement.setNomDepart("New Departement");
 
+        // Create associated Etudiant entities if needed
+        Etudiant etudiant1 = new Etudiant();
+        etudiant1.setNomE("Etudiant nom");
+        etudiant1.setPrenomE("Etudiant prenom");
+        etudiant1.setOptionE(Option.GAMIX);
+        etudiant1.setDepartement(newDepartement);
+        etudiant1.setContrats(null);
+        etudiant1.setEquipes(null);
+
+        newDepartement.setEtudiants(Collections.singletonList(etudiant1));
+
+        Departement addedDepartement = ds.add(newDepartement);
+        Assertions.assertNotNull(addedDepartement.getIdDepart());
+    }
 }
