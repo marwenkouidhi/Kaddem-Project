f15908f16db2746b1eca1a5ef99c44cde4a1ae50
samibm23 <94871825+samibm23@users.noreply.github.com>
2023-11-06 16:56:27 +0100

    'DOCKERHUB'

--- src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
@@ -37,7 +37,7 @@ class DepartementServiceImplTest {
     @Order(2)
     public void testAddDepartement() {
         Departement newDepartement = new Departement();
-        newDepartement.setNomDepart("Departement test");
+        newDepartement.setNomDepart("Departement junit");
 
         Etudiant etudiant1 = new Etudiant();
         etudiant1.setNomE("Etudiant nom");
@@ -77,7 +77,7 @@ class DepartementServiceImplTest {
     @Test
     @Order(5)
     public void deleteDepartement() {
-        Integer id = 4;
+        Integer id = 8;
         Boolean isDeleted = ds.delete(id);
         Assertions.assertTrue(isDeleted);
     }
