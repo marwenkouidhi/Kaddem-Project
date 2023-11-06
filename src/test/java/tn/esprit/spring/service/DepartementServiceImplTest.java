256d23e12f9e50df77c9d3bef3187287d348f154
samibm23 <94871825+samibm23@users.noreply.github.com>
2023-11-06 16:59:20 +0100

    'DOCKERHUB'

--- src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/DepartementServiceImplTest.java
@@ -77,7 +77,7 @@ class DepartementServiceImplTest {
     @Test
     @Order(5)
     public void deleteDepartement() {
-        Integer id = 8;
+        Integer id = 9;
         Boolean isDeleted = ds.delete(id);
         Assertions.assertTrue(isDeleted);
     }
